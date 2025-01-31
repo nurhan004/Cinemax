c0c14c2553863672d4987f35c024b1c597aeafa4
nurhan004 <sharshenbekov777@gmail.com>
2025-01-31 19:21:28 +0600

    added block1

---
+++ src/Container/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Container.css"
+import Block1 from '../components/Sections/Block1/Block1'
+
+function Container() {
+  return (
+    <div className='container'>
+        <Block1/>
+    </div>
+  )
+}
+
+export default Container
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./Home.css"
+import Block1 from '../../components/Sections/Block1/Block1'
+import Container from '../../Container/Container'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Container>
+      <Block1/>
+
+      </Container>
+    </div>
   )
 }
 
